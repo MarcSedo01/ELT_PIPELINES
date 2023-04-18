99683cf2d54a660a1aad31feb13b0d5aa98f2ddc
1564815@uab.cat <1564815@uab.cat>
2023-04-18 14:41:25 +0000

    fact orders created

--- models/marts/dim_delivery.sql
+++ models/marts/dim_delivery.sql
@@ -1,18 +1,18 @@
 
-select
+--select
 
-    seq_delivery.nextval AS DELIVERY_WID,
-    A.LIMIT_DELIVERY_HOURS,
-    A.DELIVERY_PRICE,
-    current_timestamp() AS CREATED_DATE
+   --- seq_delivery.nextval AS DELIVERY_WID,
+   --- A.LIMIT_DELIVERY_HOURS,
+   --- A.DELIVERY_PRICE,
+   --- current_timestamp() AS CREATED_DATE
 
-FROM
+---FROM
 
 (select
-
+    LIMIT_DATE_CODE,
     LIMIT_DELIVERY_HOURS,
     DELIVERY_PRICE
     
 from {{ref('staging_orders')}}
 
-group by LIMIT_DELIVERY_HOURS, DELIVERY_PRICE) A 
+group by LIMIT_DATE_CODE, LIMIT_DELIVERY_HOURS, DELIVERY_PRICE) --A 
