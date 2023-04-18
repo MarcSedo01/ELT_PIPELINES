c83ecb81584b71b3fcac5a040977fb74abf44687
1564815@uab.cat <1564815@uab.cat>
2023-04-18 13:59:36 +0000

    staging orders created

---
+++ models/staging/staging_orders.sql
@@ -0,0 +1,31 @@
+select 
+
+    ordernumber as ORDER_NUMBER,
+    quantityordered as QUANTITY_ORDERED,
+    priceeach as PRICE_EACH,
+    orderlinenumber as ORDER_LINE_NUMBER,
+    sales as SALES,
+    orderdate as ORDER_DATE,
+    SUBSTR(orderdate, 1, LENGTH(orderdate) - 5) as DATE_COLUMN,
+    TO_DATE(DATE_COLUMN) as DATE_COLUMN_,
+    TO_CHAR(DATE_COLUMN_, 'DDMMYYYY') as DATE_WID,
+    status as STATUS,
+    productline as PRODUCT_LINE,
+    msrp as MSRP,
+    productcode as PRODUCT_CODE,
+    customername as CUSTOMER_NAME,
+    phone as PHONE, 
+    addressline1 as ADDRESS_LINE,
+    city as CITY,
+    state as STATE,
+    postalcode as POSTAL_CODE,
+    country as COUNTRY,
+    territory as TERRITORY,
+    concat(contactfirstname,' ',contactlastname) as CONTACT_NAME,
+    dealsize as DEAL_SIZE,
+    deliverydate as DELIVERY_DATE,
+    limit_date_code as LIMIT_DATE_CODE,
+    limit_delivery_hours as LIMIT_DELIVERY_HOURS,
+    delivery_price as DELIVERY_PRICE
+
+from {{ref('orders')}}
